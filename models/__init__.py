fe81cf283d99aee4ee66279c21dedc0dd92452d3
soloking44 <solop8483@gmail.com>
2024-02-11 13:30:21 -0800

    Air bnb task

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-""" this establish the sile storage """
-
+"""
+this is __init__ dunder process for the models directory
+"""
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.review import Review
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.state import State
+
 
 storage = FileStorage()
 storage.reload()
